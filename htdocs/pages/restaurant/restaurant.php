f0ccb55452093e1741cc12a4d3476c575996948e
Casey Albert-Hall <case19@att.net>
2016-05-25 23:39:17 -0700

    Alpha restaurant pages

---
+++ htdocs/pages/restaurant/restaurant.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="../../style/style.css" />
+</head>
+<body>
+
+<div class = "info_page_container">
+<div class= "bigImage">
+	<image src = "../../images/restaurants/firestone.jpg" style = "width: inherit; height: inherit">
+</div>
+<div class = "info_box">
+	<h1>
+	Firestone Bar and Grill
+	</h1>
+	<ul style="list-style-type:none">	
+		<li>Location:</li> 
+		<li>Phone: </li>
+		<li>Website: </li>
+		<li>Hours: </li>
+	</ul>
+</div>
+
+<div class = "rating_box">
+	<button>Write a review</button>
+	<p>Rating: 5/5</p>
+	<p>Price: $$$</p>
+</div>
+
+<div class = "reviews">
+	<h3>User Reviews</h3>
+	<div class = "restaurant_holder">
+		<div class = "user_image">
+			<image src="../../images/users/obama.jpg" style ="height:inherit; width:inherit;">
+		</div>
+		<div class = "details">
+			<h3>Barack Obama</h3>
+			<p style="font-style:italic">"Pretty good tri tip!"</p>
+		</div>
+		<div class = "ratings">
+			<h4>Rating: 5/5</h4>
+		</div>
+	</div>
+</div>
+
+</div>
+
+
+</body>
+</html>
