fc2c6a62df389c2e1223b92970ce2b79afb4f209
tygriff <tygriffith14@yahoo.com>
2016-06-02 20:53:06 -0700

    Update listings

--- api/search_restaurants.php
+++ api/search_restaurants.php
@@ -6,52 +6,15 @@
 include_once('confi.php');
 
 $fid= isset($_POST['fid']) ? mysql_real_escape_string($_POST['fid']) :  "";
-$filter = isset($_POST['filter']) ? mysql_real_escape_string($_POST['filter']) :  "";
 $page = isset($_POST['page']) ? intval(mysql_real_escape_string($_POST['page'])) * 10 :  "";
 
-if (strcmp($filter, 'hi') == 0) {
-   $query = "SELECT R.name, R.image, R.rating, R.id FROM FoodRestaurants RF
-      JOIN Food F ON RF.foodId = F.id
-      JOIN Restaurants R ON RF.restuarantID = R.id
-      WHERE F.id = $fid
-      GROUP BY R.id
-      ORDER BY AVG(V.price) DESC
-      LIMIT $page, 10
-   ;
-   ";
-}
-else if (strcmp($filter, 'lo') == 0) {
-   $query = "SELECT R.name, R.image, R.rating, R.id FROM FoodRestaurants RF
-      JOIN Food F ON RF.foodId = F.id
-      JOIN Restaurants R ON RF.restuarantID = R.id
-      WHERE F.id = $fid
-      GROUP BY R.id
-      ORDER BY AVG(V.price)
-      LIMIT $page, 10
-   ;
-   ";
-}
-else if (strcmp($filter, 'rating') == 0) {
-   $query = "SELECT R.name, R.image, R.rating, R.id FROM FoodRestaurants RF
-      JOIN Food F ON RF.foodId = F.id
-      JOIN Restaurants R ON RF.restuarantID = R.id
-      WHERE F.id = $fid
-      GROUP BY R.id
-      ORDER BY AVG(V.rating) DESC
-      LIMIT $page, 10
-   ;
-   ";
-}
-else {
-   $query = "SELECT R.name, R.image, R.id, R.rating FROM FoodRestaurants RF
-      JOIN Food F ON RF.foodId = F.id
-      JOIN Restaurants R ON RF.restuarantID = R.id
-      WHERE F.id = $fid
-      GROUP BY R.id
-      LIMIT $page, 10
-   ;
-   ";
-}
+$query = "SELECT R.name, R.image, R.id, R.rating FROM FoodRestaurants RF
+   JOIN Food F ON RF.foodId = F.id
+   JOIN Restaurants R ON RF.restuarantID = R.id
+   WHERE F.id = $fid
+   GROUP BY R.id
+   LIMIT $page, 10
+;";
 
 $result = mysql_query($query);
 
@@ -66,15 +29,26 @@ if (mysql_num_rows($result) != 0) {
       $rating = $row['rating'];
 
       $query = "SELECT COUNT(*) AS numRatings, AVG(rating) AS rat, FLOOR(AVG(price)) AS price FROM Reviews
-         WHERE restId = 4;";
+         WHERE restId = $rid;";
       $qur = mysql_query($query);
       $restRow = mysql_fetch_assoc($qur);
       $rating = ($restRow["rat"] + ($rating * 100)) / (100 + $restRow['numRatings']);
+      $price = "";
+      if ($price == 0) {
+         $price = "NA";
+      }
+      else {
+         for ($x = 0; $x < $price; $x++) {
+            $price .= '$';
+         }
+      }
 
-
-      array_push($restaurants, array('name' => $name, 'image' => $image, 'price' => $restRow['price'],
-       'rating' => $rating, 'numRatings' => $restRow['numRatings']));
+      array_push($restaurants, array('name' => $name, 'image' => $image, 'price' => $price,
+       'rating' => round($rating,2), 'numRatings' => $restRow['numRatings'], 'rid' => $rid));
    }
+
+   //sort
+
    $json["restaurants"] = $restaurants;
    $query = "SELECT COUNT(*) AS count FROM (SELECT R.id FROM FoodRestaurants RF
       JOIN Food F ON RF.foodId = F.id
--- pages/scripts/listings.js
+++ pages/scripts/listings.js
@@ -21,20 +21,33 @@ $(document).ready(function(){
       fid = parseInt(urlInfo[1]);
    }
 
-   loadResults(fid, commentspage);
+   loadResults(fid, commentspage, uid);
    
 });
 
-function loadResults(fid, page) {
+function loadResults(fid, page, uid) {
    $.post("http://localhost:8080/SLO_Hungry/api/search_restaurants.php",
       {fid: fid,
-       page: commentspage,
-       filter: filter  
+       page: page, 
       },
       function(data) {
          var table = document.getElementById("results").getElementsByTagName('tbody')[0];
          while(table.rows.length > 0) {
            table.deleteRow(0);
+         }
+         for (var i = 0; i < data.restaurants.length; i++) {
+            var rest = data.restaurants[i];
+            var row = table.insertRow(i);
+            var image = row.insertCell(0);
+            var name = row.insertCell(1);
+            var price = row.insertCell(2);
+            var rating = row.insertCell(3);
+            var count = row.insertCell(4);
+            image.innerHTML = '<div class="imageContainer"><img src="../images/'+rest.image+'"></image></div>';
+            name.innerHTML = '<a href="http://localhost:8080/SLO_Hungry/pages/restaurant.html#'+rest.rid+'&'+uid+'">'+rest.name+'</a>';
+            price.innerHTML = rest.price;
+            rating.innerHTML = rest.rating;
+            count.innerHTML = rest.numRatings;
          }
       },
       'json'
