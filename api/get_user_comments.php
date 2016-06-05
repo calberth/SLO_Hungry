6cd96714749c77efed0b61a79c0866202d9fd49d
Casey Albert-Hall <case19@att.net>
2016-06-05 16:27:48 -0700

    profilejs

--- api/get_user_comments.php
+++ api/get_user_comments.php
@@ -63,7 +63,7 @@ if (mysql_num_rows($result) != 0) {
    $json["comments"] = $comments;
 }
 else {
-   $json = array("status" => 0, "msg" => "No Comments $page  $uId");
+   $json = array("status" => 0, "msg" => "No Comments $page  $uId", "count" => 0);
 }
 
 @mysql_close($conn);
